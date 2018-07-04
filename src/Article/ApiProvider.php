e9266d55326d8a44da2a0d6df9e7700027d95570
moulaye <moulaye.c@gmail.com>
2018-07-04 12:10:44 +0200

    Implementing Mediator

---
+++ src/Article/ApiProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moula
+ * Date: 04/07/2018
+ * Time: 11:56
+ */
+
+namespace App\Article;
+
+
+class ApiProvider
+{
+
+}
---
+++ src/Article/ArticleCollection.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moula
+ * Date: 04/07/2018
+ * Time: 11:58
+ */
+
+namespace App\Article;
+
+
+use Countable;
+use Iterator;
+
+class ArticleCollection implements Countable
+{
+    /**
+     * @var array $articles
+     */
+    private $articles;
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return $this->count($this->articles);
+    }
+}
